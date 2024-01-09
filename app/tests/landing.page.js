f5cc295a36ec6cdd759157bfed178232b5cc8b34
willjsimmons <willjsimmons@gmail.com>
2024-01-08 18:05:41 -1000

    Initial commit

---
+++ app/tests/landing.page.js
@@ -0,0 +1,18 @@
+import { Selector, t } from 'testcafe';
+import { PAGE_IDS } from '../imports/ui/utilities/PageIDs';
+
+class LandingPage {
+  constructor() {
+    this.pageId = `#${PAGE_IDS.LANDING}`;
+    this.pageSelector = Selector(this.pageId);
+  }
+
+  /* Asserts that this page is currently displayed. */
+  async isDisplayed() {
+    // From https://testcafe.io/documentation/402803/recipes/best-practices/create-helpers
+    // Note that this file imports t (the test controller) from the testcafe module. You don’t need to pass t to helper functions because TestCafe can resolve the current test context and provide the correct test controller instance.
+    await t.expect(this.pageSelector.exists).ok();
+  }
+}
+
+export const landingPage = new LandingPage();
