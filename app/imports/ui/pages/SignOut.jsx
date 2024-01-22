2affcf1db40d3b20134ec4c96470cf38bced230b
aldenparoni <aparoni@hawaii.edu>
2024-01-22 11:22:13 -1000

    add buttons that direct to the landing page and the sign-in page

--- app/imports/ui/pages/SignOut.jsx
+++ app/imports/ui/pages/SignOut.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Col } from 'react-bootstrap';
+import { Container, Col, Button } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
@@ -9,7 +9,9 @@ const SignOut = () => {
   return (
     <Container id={PAGE_IDS.SIGN_OUT} className="justify-content-center py-3">
       <Col className="text-center py-3">
-        <h2>You are signed out. Thank you for using the Voluntree!</h2>
+        <h2>You are signed out.</h2>
+        <Button href="/" className="m-2">Home</Button>
+        <Button href="signin" className="m-2">Sign in</Button>
       </Col>
     </Container>
   );
