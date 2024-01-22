a4b5363caa442a57c2fd00d989c353a33e4aad6b
thomasarivera <thomas5info@gmail.com>
2024-01-21 14:08:15 -1000

    Added subscription to Eventcollection

--- app/imports/ui/components/SearchBar.jsx
+++ app/imports/ui/components/SearchBar.jsx
@@ -1,23 +1,45 @@
+import React from 'react';
 import Fuse from 'fuse.js';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+import { Events } from '../../api/event/EventCollection';
+import LoadingSpinner from './LoadingSpinner';
 
 const SearchBar = () => {
 
-  const fuseOptions = {
-    isCaseSensitive: false,
-    shouldSort: true,
-    includeMatches: true,
-    findAllMatches: true,
-    useExtendedSearch: false,
-    keys: [
-      'Event', 'Organization',
-    ],
-  };
+  const { event, ready } = useTracker(() => {
+    const subscription = Meteor.subscribe(Events.userPublicationName);
+    const rdy = subscription.ready();
+    const item = Events.collection.find({}).fetch();
+    return {
+      event: item,
+      ready: rdy,
+    };
+  }, []);
 
-  const fuse = new Fuse(Event, fuseOptions);
+  if (ready && event) {
 
-  const result = fuse.search('');
+    const fuseOptions = {
+      isCaseSensitive: false,
+      shouldSort: true,
+      includeMatches: true,
+      findAllMatches: true,
+      useExtendedSearch: false,
+      keys: [
+        'Event', 'Organization',
+      ],
+    };
 
-  return result;
+    const fuse = new Fuse(Event, fuseOptions);
+
+    const result = fuse.search('');
+
+    return result;
+
+  }
+  return (
+    <LoadingSpinner />
+  );
 };
 
 export default SearchBar;
