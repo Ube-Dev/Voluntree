40145cf86dfb6b2221a4e8df757f7191f6f7dd36
thomasarivera <thomas5info@gmail.com>
2024-01-21 20:34:29 -1000

    Created mock up search bar and filters

--- app/imports/ui/components/SearchBar.jsx
+++ app/imports/ui/components/SearchBar.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Dropdown, Form } from 'react-bootstrap';
 import Fuse from 'fuse.js';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Events } from '../../api/event/EventCollection';
 import LoadingSpinner from './LoadingSpinner';
 
 const SearchBar = () => {
+
+  const [searchQuery, setSearchQuery] = useState('');
 
   const { ready, events } = useTracker(() => {
     // Note that this subscription will get cleaned up
@@ -20,10 +23,11 @@ const SearchBar = () => {
       ready: rdy,
     };
   }, []);
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
   if (ready) {
-    console.log('Ready');
-    console.log(events);
     const fuseOptions = {
       isCaseSensitive: false,
       shouldSort: true,
@@ -35,10 +39,37 @@ const SearchBar = () => {
 
     if (events && events.length > 0) {
       const fuse = new Fuse(events, fuseOptions);
-      const result = fuse.search('Humanitarian');
+      const result = fuse.search(searchQuery);
 
       return (
         <div>
+          <Form.Group controlId="formEventSearch" className="d-flex align-items-center">
+            <Form.Control type="text" placeholder="Search for events..." style={{ width: '500px' }} className="mr-2" value={searchQuery} onChange={handleSearchChange} />
+            {/* Filter Dropdowns */}
+            <Dropdown>
+              <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                Select Distance
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">5 miles</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">10 miles</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">20 miles</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <Dropdown>
+              <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                Select Organization
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Organization 1</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Organization 2</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Organization 3</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <Button variant="secondary" type="submit" className="ml-2">
+              Search
+            </Button>
+          </Form.Group>
           <ul>
             {result.map((item) => (
               <li key={item.item._id}>
@@ -53,7 +84,6 @@ const SearchBar = () => {
     return (
       <p>No events found</p>
     );
-
   }
 
   return (
