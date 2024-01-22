284dde7b5f8a3b41cf94e6e9311a8e3352c4c7b1
thomasarivera <thomas5info@gmail.com>
2024-01-22 08:27:29 -1000

    Updated Landing page with the voluntree logo and buttons

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Image, Button } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* A simple static component to render some text for the landing page. */
@@ -7,8 +7,18 @@ const Landing = () => (
   <Container id={PAGE_IDS.LANDING} className="py-3">
     <Row className="align-middle text-center">
       <Col xs={12} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to Voluntree</h1>
-        <p>Now get to work and modify this app!</p>
+        <Image src="/images/voluntree.png" alt="Voluntree Logo" className="mx-auto d-block" style={{ width: '45%' }} />
+        <h1>Working together made simple</h1>
+      </Col>
+    </Row>
+    <Row className="mb-5 g-0 justify-content-center">
+      <Col xs="auto">
+        <Button className="rounded-0" variant="outline-light-landing" size="lg" href="/signin">
+          Sign In
+        </Button>
+        <Button className="rounded-0" variant="light" size="lg" href="/signup">
+          Register
+        </Button>
       </Col>
     </Row>
   </Container>
