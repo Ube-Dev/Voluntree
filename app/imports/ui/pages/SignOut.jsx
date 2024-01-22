bc88ba099d9b64c2459569a3c00010a45abf7f3a
aldenparoni <aparoni@hawaii.edu>
2024-01-22 11:08:26 -1000

    add custom colors to css, begin work on sign-out page

--- app/imports/ui/pages/SignOut.jsx
+++ app/imports/ui/pages/SignOut.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import Col from 'react-bootstrap/Col';
+import { Container, Col } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const SignOut = () => {
   Meteor.logout();
   return (
-    <Col id={PAGE_IDS.SIGN_OUT} className="text-center py-3"><h2>You are signed out.</h2></Col>
+    <Container id={PAGE_IDS.SIGN_OUT} className="justify-content-center py-3">
+      <Col className="text-center py-3">
+        <h2>You are signed out. Thank you for using the Voluntree!</h2>
+      </Col>
+    </Container>
   );
 };
 
