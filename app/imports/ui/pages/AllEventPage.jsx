4b21bba57a48193fc195a531a10c6eeaa5650bf7
John <liujl@hawaii.edu>
2024-01-24 01:43:19 -1000

    *added simple pagination component from react-bootstrap. *styled all the containers to the center.
    issue:
    *needs to do the logic for the pagination bar.

--- app/imports/ui/pages/AllEventPage.jsx
+++ app/imports/ui/pages/AllEventPage.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { Container, Card, Button, ButtonGroup, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Card, Button, ButtonGroup, Row, Col, Pagination } from 'react-bootstrap';
 import { TagFill } from 'react-bootstrap-icons';
 
 // import EventCard from '../components/EventCard';
@@ -9,40 +9,45 @@ import '../css/AllEventPage.css';
 import CommitToEvent from '../components/CommitToEvent';
 // samples for display purposes.
 const img_path = '/images/workTogetherAbout.png';
-const events = {
-  1: {
-    key: 1,
-    img: img_path,
-    title: 'event',
-    description: 'this is a description',
-    date: '01-16-24',
-    requiredSkills: ['skill1', 'skill2', 'skill3'],
+const sample_data = {
+  events: {
+    1: {
+      key: 1,
+      img: img_path,
+      title: 'event',
+      description: 'this is a description',
+      date: '01-16-24',
+      requiredSkills: ['skill1', 'skill2', 'skill3'],
+    },
+    2: {
+      key: 2,
+      img: img_path,
+      title: 'event',
+      description: 'this is a description',
+      date: '01-16-24',
+      requiredSkills: ['skill1', 'skill2', 'skill3'],
+    },
+    3: {
+      key: 3,
+      img: img_path,
+      title: 'long event',
+      // eslint-disable-next-line max-len
+      description: 'this is a loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooon description',
+      date: '01-16-24',
+      // eslint-disable-next-line max-len
+      requiredSkills: ['skill1', 'skill2', 'skill3', 'skill1', 'skill2', 'skill3', 'skill1', 'skill2', 'skill3'],
+    },
+    4: {
+      key: 4,
+      img: img_path,
+      title: 'event',
+      description: 'this is a description',
+      date: '01-16-24',
+      requiredSkills: ['skill1', 'skill2', 'skill3'],
+    },
   },
-  2: {
-    key: 2,
-    img: img_path,
-    title: 'event',
-    description: 'this is a description',
-    date: '01-16-24',
-    requiredSkills: ['skill1', 'skill2', 'skill3'],
-  },
-  3: {
-    key: 3,
-    img: img_path,
-    title: 'long event',
-    // eslint-disable-next-line max-len
-    description: 'this is a loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooon description',
-    date: '01-16-24',
-    // eslint-disable-next-line max-len
-    requiredSkills: ['skill1', 'skill2', 'skill3', 'skill1', 'skill2', 'skill3', 'skill1', 'skill2', 'skill3'],
-  },
-  4: {
-    key: 4,
-    img: img_path,
-    title: 'event',
-    description: 'this is a description',
-    date: '01-16-24',
-    requiredSkills: ['skill1', 'skill2', 'skill3'],
+  pagination: {
+    totalPages: 10,
   },
 };
 
@@ -52,14 +57,15 @@ const AllEventPage = () => {
   const filterSkill = (skill) => {
     // console.log('triggered');
   };
+
   return (
     <>
-      <Container className="align-items-center justify-content-center">
+      <Container className="d-flex justify-content-center mt-3">
         <SearchBar />
       </Container>
       <Container>
         <Row className="g-2">
-          {Object.values(events).map((event) => (
+          {Object.values(sample_data.events).map((event) => (
             <Col className="w-25 h-25 xl-3 md-4 sm-6">
               <Card key={event.key} className="">
                 <Card.Img variant="top" src={event.img} />
@@ -87,6 +93,25 @@ const AllEventPage = () => {
 
           ))}
         </Row>
+      </Container>
+      <Container className="d-flex justify-content-center mt-3">
+        <Pagination>
+          <Pagination.First />
+          <Pagination.Prev />
+          <Pagination.Item>{1}</Pagination.Item>
+          <Pagination.Ellipsis />
+
+          <Pagination.Item>{10}</Pagination.Item>
+          <Pagination.Item>{11}</Pagination.Item>
+          <Pagination.Item active>{12}</Pagination.Item>
+          <Pagination.Item>{13}</Pagination.Item>
+          <Pagination.Item disabled>{14}</Pagination.Item>
+
+          <Pagination.Ellipsis />
+          <Pagination.Item>{20}</Pagination.Item>
+          <Pagination.Next />
+          <Pagination.Last />
+        </Pagination>
       </Container>
     </>
   );
