f4f2aee55c4582dc6f61e88ddb1c3b968cc2a2d3
aldenparoni <aparoni@hawaii.edu>
2024-01-31 11:01:15 -1000

    Create OrgProfileCard.jsx and UserProfileCard.jsx

---
+++ app/imports/ui/components/OrgProfileCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import '../css/EventCard.css';
+
+const OrgProfileCard = () => (
+  <Container className="py-3">
+    <Row className="justify-content-center">
+      <h1>Organization Profile</h1>
+    </Row>
+  </Container>
+);
+
+export default OrgProfileCard;
--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import { Container } from 'react-bootstrap';
+import { Roles } from 'meteor/alanning:roles';
+import { ROLE } from '../../api/role/Role';
+import UserProfileCard from '../components/UserProfileCard';
+import OrgProfileCard from '../components/OrgProfileCard';
 
 const Profile = () => (
-  <Container>
-    <h1>Profile Page</h1>
+  <Container className="py-3">
+    {(Roles.userIsInRole(Meteor.userId(), [ROLE.USER]) || Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN])) ? (
+      <UserProfileCard />
+    ) : ''}
+    {Roles.userIsInRole(Meteor.userId(), [ROLE.ORGANIZATION]) ? (
+      <OrgProfileCard />
+    ) : ''}
   </Container>
 );
 
