61ac2800987dc860bed4333e1772d80cfa6d18f8
aldenparoni <aparoni@hawaii.edu>
2024-01-31 11:43:19 -1000

    Add AccountAdded.jsx to repo and add path to the page

---
+++ app/imports/ui/pages/AccountAdded.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const AccountAdded = () => (
+  <Container>
+    <h1>Account Added</h1>
+  </Container>
+);
+
+export default AccountAdded;
