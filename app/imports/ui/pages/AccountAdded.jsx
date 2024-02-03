577fda708899abba9bbd0d37d4bd422d70781aea
aldenparoni <aparoni@hawaii.edu>
2024-02-02 19:20:34 -1000

    Update AccountAdded.jsx

--- app/imports/ui/pages/AccountAdded.jsx
+++ app/imports/ui/pages/AccountAdded.jsx
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 
 const AccountAdded = () => (
   <Container>
-    <h1>Account Added</h1>
+    <h1>Your account has been created successfully.</h1>
   </Container>
 );
 
