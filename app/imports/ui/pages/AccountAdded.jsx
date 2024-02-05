3eb9324485f77999fd1d4cd950b9bc6d67237c82
aldenparoni <aparoni@hawaii.edu>
2024-02-05 10:02:02 -1000

    Add Home button

--- app/imports/ui/pages/AccountAdded.jsx
+++ app/imports/ui/pages/AccountAdded.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 const AccountAdded = () => (
   <Container className="py-3">
     <Row className="align-items-center justify-content-center">
       <Col className="text-center">
         <h1>Account created successfully. Welcome to the Voluntree!</h1>
+        <Button href="/home" className="m-2">Home</Button>
       </Col>
     </Row>
   </Container>
