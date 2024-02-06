6539db88b0732c89588628770a3453c555c7e279
aldenparoni <aparoni@hawaii.edu>
2024-02-06 12:10:17 -1000

    Create UserProfile.jsx

---
+++ app/imports/ui/pages/UserProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+
+const UserProfile = () => (
+  <Container fluid className="justify-content-center">
+    <Row className="text-center align-items-center">
+      <h1>User Profile</h1>
+    </Row>
+  </Container>
+);
+
+export default UserProfile;
