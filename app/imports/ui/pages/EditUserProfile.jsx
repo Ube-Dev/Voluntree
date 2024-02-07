6e89fe0fb7f909a45c117e94aab7c143b8a1c58c
aldenparoni <aparoni@hawaii.edu>
2024-02-06 20:04:14 -1000

    Update EditUserProfile.jsx

--- app/imports/ui/pages/EditUserProfile.jsx
+++ app/imports/ui/pages/EditUserProfile.jsx
@@ -15,7 +15,8 @@ const EditUserProfile = () => {
   // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
   const { _id } = useParams();
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { doc, ready } = useTracker(() => {
+  /* const { doc, ready } = useTracker(() => {
+    // const currentUser = Meteor.user();
     // Get access to Profile documents.
     const subscription = UserProfiles.subscribeUser();
     // Determine if the subscription is ready
@@ -26,11 +27,20 @@ const EditUserProfile = () => {
       doc: document,
       ready: rdy,
     };
-  }, [_id]);
+  }, [_id]); */
+  const { ready, userProfile } = useTracker(() => {
+    const currentUser = Meteor.user(); // Retrieve the current user
+    const subscription = currentUser ? UserProfiles.subscribeUser() : null; // Subscribe to userProfile publication for the current user
+    const profile = currentUser ? UserProfiles.findOne({ userID: currentUser._id }) : null; // Query user profile for the current user
+    return {
+      ready: subscription ? subscription.ready() : false,
+      userProfile: profile,
+    };
+  });
   // On successful submit, insert the data.
   const submit = (data) => {
     const { firstName, lastName, username, description, profilePictureLink } = data;
-    UserProfiles.collection.update(_id, { $set: { firstName, lastName, username, description, profilePictureLink } }, (error) => (error ?
+    UserProfiles.update(_id, { $set: { firstName, lastName, username, description, profilePictureLink } }, (error) => (error ?
       swal('Error', error.message, 'error') :
       swal('Success', 'Profile updated successfully.', 'success')));
   };
@@ -39,7 +49,7 @@ const EditUserProfile = () => {
       <Row className="justify-content-center">
         <Col xs={10}>
           <Col className="pb-2 text-center"><h2>Edit Profile</h2></Col>
-          <AutoForm schema={bridge} onSubmit={data => submit(data)} model={doc}>
+          <AutoForm schema={bridge} onSubmit={data => submit(data)} model={userProfile}>
             <Card>
               <Card.Body>
                 <Row>
