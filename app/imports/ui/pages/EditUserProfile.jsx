79c9b4dd323e3bcf16e61b0d7609e1dae5a4dd45
aldenparoni <aparoni@hawaii.edu>
2024-02-06 18:23:01 -1000

    Update EditUserProfile.jsx

--- app/imports/ui/pages/EditUserProfile.jsx
+++ app/imports/ui/pages/EditUserProfile.jsx
@@ -1,12 +1,68 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import swal from 'sweetalert';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, HiddenField, LongTextField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { useParams } from 'react-router';
+import LoadingSpinner from '../components/LoadingSpinner';
+import { UserProfiles } from '../../api/user/UserProfileCollection';
 
-const EditUserProfile = () => (
-  <Container fluid className="justify-content-center">
-    <Row className="text-center align-items-center">
-      <h1>Edit User Profile</h1>
-    </Row>
-  </Container>
-);
+const bridge = new SimpleSchema2Bridge(UserProfiles.schema);
+
+const EditUserProfile = () => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const { _id } = useParams();
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { doc, ready } = useTracker(() => {
+    // Get access to Profile documents.
+    const subscription = Meteor.subscribe(UserProfiles.userPublications);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the document
+    const document = UserProfiles.collection.findOne(_id);
+    return {
+      doc: document,
+      ready: rdy,
+    };
+  }, [_id]);
+  // On successful submit, insert the data.
+  const submit = (data) => {
+    const { firstName, lastName, username, description, profilePictureLink } = data;
+    UserProfiles.collection.update(_id, { $set: { firstName, lastName, username, description, profilePictureLink } }, (error) => (error ?
+      swal('Error', error.message, 'error') :
+      swal('Success', 'Profile updated successfully.', 'success')));
+  };
+  return ready ? (
+    <Container className="py-3 formCSS" id="edit-profile-page">
+      <Row className="justify-content-center">
+        <Col xs={10}>
+          <Col className="pb-2 text-center"><h2>Edit Profile</h2></Col>
+          <AutoForm schema={bridge} onSubmit={data => submit(data)} model={doc}>
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col><TextField name="firstName" label="First Name" /></Col>
+                  <Col><TextField name="lastName" label="Last Name" /></Col>
+                </Row>
+                <Row>
+                  <Col><TextField name="username" /></Col>
+                  <Col><TextField name="profilePictureLink" label="Profile Picture" /></Col>
+                </Row>
+                <Row>
+                  <Col><LongTextField name="description" label="Your Bio" /></Col>
+                </Row>
+                <SubmitField value="Submit" />
+                <ErrorsField />
+                <HiddenField name="owner" />
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />;
+};
 
 export default EditUserProfile;
