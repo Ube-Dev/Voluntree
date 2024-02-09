c28887d8927ccdc46cc554a207f87025e149027a
thomasarivera <thomas5info@gmail.com>
2024-02-09 12:59:22 -1000

    Updated Navbar: Removed About and FAQ

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -5,19 +5,20 @@ import { Container, Col, Row, Image } from 'react-bootstrap';
 const Footer = () => {
   const divStyle = { paddingTop: '15px' };
   return (
-    <footer className="mt-auto mb-2 bg-light">
+    <footer className="mt-2 mb-2 bg-light">
       <Container style={divStyle}>
         <Row>
-          <Col>
+          <Col sm={12} md={4}>
             <hr />
             <Image src="/images/voluntreeLanding1.png" width="150px" />
           </Col>
-          <Col>
+          <Col sm={12} md={4}>
             <hr />
             <a href="/about">About Us</a> <br />
             <a href="/FAQ">FAQ</a> <br />
+            <a href="/Events">Find Events</a> <br />
           </Col>
-          <Col className="text-start">
+          <Col sm={12} md={4} className="text-start">
             <hr />
             Created by <a href="https://ube-dev.github.io/">Ube-Dev</a> <br />
             Based on a template provided by <br />
