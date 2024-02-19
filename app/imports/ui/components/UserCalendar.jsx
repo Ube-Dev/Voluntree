508b12853c7672cdb05b9a269e799bf69c76e1be
Hok Wai Chan <hokwai@hawaii.edu>
2024-02-19 03:01:23 -1000

    Remove big calendar

--- app/imports/ui/components/UserCalendar.jsx
+++ app/imports/ui/components/UserCalendar.jsx
@@ -1,73 +1,161 @@
 import React, { useState } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import PropTypes from 'prop-types';
-import moment from 'moment';
-import { Card } from 'react-bootstrap';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-
-const localize = momentLocalizer(moment);
-
-const Toolbar = ({ label, onNavigate, onView }) => (
-  <div className="rbc-toolbar">
-    <span className="rbc-btn-group">
-      <button type="button" onClick={() => onNavigate('PREV')}>
-        {'<'}
-      </button>
-      <button type="button" onClick={() => onNavigate('TODAY')}>
-        Today
-      </button>
-      <button type="button" onClick={() => onNavigate('NEXT')}>
-        {'>'}
-      </button>
-    </span>
-    <span className="rbc-toolbar-label" style={{ fontSize: '30px', color: 'black' }}>
-      {label}
-    </span>
-    <span className="rbc-btn-group">
-      <button type="button" onClick={() => onView('month')}>
-        Month
-      </button>
-      <button type="button" onClick={() => onView('day')}>
-        Day
-      </button>
-    </span>
-  </div>
-);
-
-Toolbar.propTypes = {
-  label: PropTypes.string.isRequired,
-  onNavigate: PropTypes.func.isRequired,
-  onView: PropTypes.func.isRequired,
-};
+import { Table, Button, Modal, Form, Card, Container } from 'react-bootstrap';
 
 const UserCalendar = () => {
-  const [events, setEvents] = useState([]);
 
-  const handleSelect = ({ start, end }) => {
-    const title = window.prompt('Add event');
-    if (title) {
-      const newEvent = { start, end, title };
-      setEvents([...events, newEvent]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [showModal, setShowModal] = useState(false);
+  const [events, setEvents] = useState({});
+
+  const getDaysInMonth = (year, month) => new Date(year, month + 1, 0).getDate();
+
+  const getMonthData = (year, month) => {
+    const firstDay = new Date(year, month, 1);
+    const daysInMonth = getDaysInMonth(year, month);
+
+    const data = [];
+    let dayCounter = 1;
+
+    for (let i = 0; i < 6; i++) {
+      const week = [];
+      for (let j = 0; j < 7; j++) {
+        if ((i === 0 && j < firstDay.getDay()) || dayCounter > daysInMonth) {
+          week.push(null);
+        } else {
+          week.push(new Date(year, month, dayCounter));
+          dayCounter++;
+        }
+      }
+      data.push(week);
     }
+    return data;
   };
 
-  return (
-    <Card style={{ width: 'calc(100% - 20px)', height: 'calc(100% - 20px)', padding: '10px' }}>
-      <div style={{ width: '100%', height: '100%', backgroundColor: 'white', color: 'black' }}>
-        <Calendar
-          localizer={localize}
-          events={events}
-          startAccessor="start"
-          endAccessor="end"
-          selectable
-          onSelectSlot={handleSelect}
-          components={{
-            toolbar: Toolbar,
-          }}
-          style={{ color: 'black', width: 'calc(100% - 20px)', height: 'calc(100% - 20px)', margin: '10px' }}
-        />
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleEventChange = (event) => {
+    const { value } = event.target;
+    setEvents({
+      ...events,
+      [selectedDate.toDateString()]: value,
+    });
+  };
+
+  const handleSaveEvent = () => {
+    setShowModal(false);
+  };
+
+  const changeMonth = (offset) => {
+    setSelectedDate(new Date(selectedDate.getFullYear(), selectedDate.getMonth() + offset, 1));
+  };
+
+  const renderHeader = () => {
+    const options = { month: 'long', year: 'numeric' };
+    return (
+      <div className="d-flex justify-content-between align-items-center mb-2">
+        <Button variant="link" onClick={() => changeMonth(-1)}>
+          {'<'}
+        </Button>
+        <h2>{selectedDate.toLocaleDateString(undefined, options)}</h2>
+        <Button variant="link" onClick={() => changeMonth(1)}>
+          {'>'}
+        </Button>
       </div>
-    </Card>
+    );
+  };
+
+  const renderDaysOfWeekHeader = () => {
+    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    return (
+      <thead>
+        <tr>
+          {daysOfWeek.map((day, index) => (
+            <th style={{ fontSize: '20px', padding: '10px' }} key={index} className="text-center">{day}</th>
+          ))}
+        </tr>
+      </thead>
+    );
+  };
+
+  const renderCalendarBody = () => {
+    const monthData = getMonthData(selectedDate.getFullYear(), selectedDate.getMonth());
+    return (
+      <tbody>
+        {monthData.map((week, weekIndex) => (
+          <tr key={weekIndex}>
+            {week.map((day, dayIndex) => (
+              <td
+                key={dayIndex}
+                role="gridcell"
+                className={`${day ? 'cursor-pointer' : 'empty-day'}`}
+                style={{ width: '110px', height: '80px' }}
+                onClick={() => day && handleDateClick(day)}
+              >
+                {day && (
+                  <div className="day-content">
+                    <span className="day-number">{day.getDate()}</span>
+                    {events[day.toDateString()] && (
+                      <div
+                        role="button"
+                        tabIndex={0}
+                        onClick={() => handleDateClick(day)}
+                        onKeyDown={(e) => e.key === 'Enter' && handleDateClick(day)}
+                      >
+                        {events[day.toDateString()]}
+                      </div>
+                    )}
+                  </div>
+                )}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  };
+
+  const renderModal = () => (
+    <Modal show={showModal} onHide={handleModalClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>{selectedDate.toLocaleDateString()}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form.Control
+          as="textarea"
+          placeholder="Add events..."
+          value={events[selectedDate.toDateString()] || ''}
+          onChange={handleEventChange}
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleModalClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleSaveEvent}>
+          Save
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+
+  return (
+    <Container fluid>
+      <Card className="p-3">
+        {renderHeader()}
+        <Table bordered responsive="md" size="sm">
+          {renderDaysOfWeekHeader()}
+          {renderCalendarBody()}
+        </Table>
+        {renderModal()}
+      </Card>
+    </Container>
   );
 };
 
