1afa9e56e9bdb81818b84efda024eecc84a93e9d
sierranmorales <143547246+sierranmorales@users.noreply.github.com>
2024-02-20 21:55:19 -1000

    Update MyEventPage.jsx
    Adjust MyEventPage.jsx to use EventCardWithActions

--- app/imports/ui/pages/MyEventPage.jsx
+++ app/imports/ui/pages/MyEventPage.jsx
@@ -15,59 +15,7 @@ import { UserProfiles } from '../../api/user/UserProfileCollection';
 import EventCard from '../components/EventCard';
 import EventList from '../components/EventList';
 
-const MyEventPage = () => {
-  const { ready, events } = useTracker(() => {
-    // Get the current user
-    const currentUser = Meteor.user();
-    if (!currentUser) {
-      return {
-        events: [],
-        ready: false,
-      };
-    }
 
-    // Subscribe to events and userProfile
-    const eventSubscription = Events.subscribeEvent();
-    const userSubscription = UserProfiles.subscribeUser();
-    const rdy = eventSubscription.ready() && userSubscription.ready();
-
-    // Get user info
-    const theUser = UserProfiles.findOne({ email: currentUser.username });
-    // Ensure onGoingEvents is an array
-    const onGoingEvents = Array.isArray(theUser?.onGoingEvents) ? theUser.onGoingEvents : [];
-    // Fetch all events that the user is attending/registered for
-    const userEvents = Events.find({ _id: { $in: onGoingEvents } }).fetch();
-
-    return {
-      events: userEvents,
-      ready: rdy,
-    };
-  }, []);
-
-  // Show a loading indicator or any placeholder content while the data is loading
-  if (!ready) {
-    return <LoadingSpinner />;
-  }
-
-  // Render the event cards once it is ready
-  if (events && events.length > 0) {
-    return (
-      <Container>
-        {events.map(event => (
-          <EventCardWithActions key={event._id} event={event} />
-        ))}
-      </Container>
-    );
-  }
-
-  // If not, render page not found
-  return <NotFound />;
-};
-
-export default MyEventPage;
-
-
-/*
 const MyEventPage = () => {
   const { ready, events } = useTracker(() => {
     // get current user
@@ -105,9 +53,9 @@ const MyEventPage = () => {
   if (events && events.length > 0) {
     return <EventList theEvents={events} />;
   }
+
   // if not, render page not found
   return <NotFound />;
 };
 
 export default MyEventPage;
- */
