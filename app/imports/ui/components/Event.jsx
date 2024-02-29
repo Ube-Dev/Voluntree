73cb153558020494fdef30048dc02725b096a36e
thomasarivera <thomas5info@gmail.com>
2024-02-28 21:08:18 -1000

    Adjusted font size

--- app/imports/ui/components/Event.jsx
+++ app/imports/ui/components/Event.jsx
@@ -1,57 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row, Card } from 'react-bootstrap';
 import { Bell, Calendar, GeoAlt } from 'react-bootstrap-icons';
 import CommitToEvent from './CommitToEvent';
 
 const Event = ({ event }) => (
   <Container id="view-event-page" className="py-3 d-flex">
-    <Row className="justify-content-center pt-3">
-      <Col>
-        <Container>
+    <Card className="rounded-4">
+      <Container className="p-1">
+        <Row className="justify-content-center pt-3">
           <Col>
-            <h1>{event.title}</h1>
-            <h5>Organization: {event.hostBy}</h5>
-            <hr />
-            <Col>
-              <Image className="pt-5" src={event.image} width={600} style={{ objectFit: 'cover' }} />
-              <h5 className="pt-3">{event.description}</h5>
-            </Col>
+            <Container>
+              <Col>
+                <h1>{event.title}</h1>
+                <h5>Organization: {event.hostBy}</h5>
+                <hr />
+                <Col>
+                  <Image className="pt-1 justify-content-center" src={event.image} width="500" />
+                  <h5>{event.description}</h5>
+                </Col>
+              </Col>
+            </Container>
           </Col>
-        </Container>
-      </Col>
-      <Col>
-        <Container className="pt-5">
-          <Row className="pb-1">
-            <h3><GeoAlt /> {event.address}, {event.zipCode}, {event.city}, {event.state}, {event.country}</h3>
-            <br />
-          </Row>
-          <Row className="pb-1">
-            <h3><Calendar /> {event.time.toLocaleDateString()}</h3>
-            <br />
-          </Row>
-          <Row className="pb-1">
-            <h3><Bell /> Frequency: {event.frequency}</h3>
-            <hr />
-          </Row>
-          <Row className="pb-1 pt-5">
-            <h3>Accessible to: {event.accessibilities.join(', ')}</h3>
-          </Row>
-          <Row className="pb-1 pt-5">
-            <h3>Required Skills: {event.requiredSkills.join(', ')}</h3>
-          </Row>
-          <Row className="pb-1 pt-5">
-            <h3>Requires: {event.requirements.join(', ')}</h3>
-          </Row>
-          <Row className="pb-1 pt-5">
-            <h3>Impact: {event.impact}</h3>
-          </Row>
-          <Row className="pb-1 pt-5">
-            <CommitToEvent event={event} />
-          </Row>
-        </Container>
-      </Col>
-    </Row>
+          <Col>
+            <Container className="pt-1">
+              <Row className="pt-1">
+                <h3><GeoAlt /> {event.address}, {event.zipCode}, {event.city}, {event.state}, {event.country}</h3>
+                <br />
+              </Row>
+              <Row className="pt-1">
+                <h3><Calendar /> {event.time.toLocaleDateString()}</h3>
+                <br />
+              </Row>
+              <Row className="pt-1">
+                <h3><Bell /> Frequency: {event.frequency}</h3>
+              </Row>
+              <Row className="pt-1">
+                <h3>Accessible to:</h3>
+                <h5>{event.accessibilities.join(', ')}</h5>
+              </Row>
+              <Row className="pt-1">
+                <h3>Required Skills:</h3>
+                <h5>{event.requiredSkills.join(', ')}</h5>
+              </Row>
+              <Row className="pt-1">
+                <h3>Requires:</h3>
+                <h5>{event.requirements.join(', ')}</h5>
+              </Row>
+              <Row className="pt-1">
+                <h3>Impact:</h3>
+                <h5>{event.impact}</h5>
+              </Row>
+              <Row className="p-1">
+                <Col className="align-content-center justify-content-start center">
+                  <CommitToEvent event={event} />
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </Card>
   </Container>
 );
 
