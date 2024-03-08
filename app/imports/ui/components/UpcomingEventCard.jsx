b98a556fdb229150e4ae412360f2b869d119e751
Hok Wai Chan <hokwai@hawaii.edu>
2024-03-08 02:39:25 -1000

    Styling UpcomingEventCard

--- app/imports/ui/components/UpcomingEventCard.jsx
+++ app/imports/ui/components/UpcomingEventCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Row, Col, Image, Button, Container } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import { UserProfiles } from '../../api/user/UserProfileCollection';
 import { Events, eventPublications } from '../../api/event/EventCollection';
 import LoadingSpinner from './LoadingSpinner';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
+import CommitToEvent from './CommitToEvent';
+import '../css/UpComingEventCard.css';
 
 const UpcomingEventCard = () => {
   const { ready, userProfile } = useTracker(() => {
@@ -26,19 +28,27 @@ const UpcomingEventCard = () => {
       <Card.Header>
         <h2>Upcoming Events</h2>
       </Card.Header>
-      <Card.Body className="d-flex justify-content-start align-items-start p-2">
+      <Card.Body className="p-0">
         {userProfile && userProfile.onGoingEvents && userProfile.onGoingEvents.length > 0 ? (
-          <ul>
-            {userProfile.onGoingEvents.map(eventId => {
-              const event = Events.findOne({ _id: eventId });
-              return event ? (
-                <li key={eventId}>
-                  <a href={`/view_event/${eventId}`}>{event.title}</a>
-                  <p>{event.startTime.toLocaleDateString()}</p>
-                </li>
-              ) : null;
-            })}
-          </ul>
+          userProfile.onGoingEvents.map((eventId) => {
+            const event = Events.findOne({ _id: eventId });
+            return event ? (
+              <Container>
+                <Row key={eventId} className="event-border">
+                  <Col xs={4} className="event-image">
+                    <Image src={event.image} fluid rounded />
+                  </Col>
+                  <Col xs={8} className="d-flex flex-column">
+                    <div className="event-date">{event.startTime.toLocaleDateString()}</div>
+                    <a className="event-title" href={`/view_event/${eventId}`}>{event.title}</a>
+                    <div className="event-button d-flex justify-content-end align-items-center mt-auto">
+                      <CommitToEvent event={event} />
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            ) : null;
+          })
         ) : (
           <p className="m-0">Hmmm... No Events...</p>
         )}
