b9f91d42614626865088e8b8e575da309cba420b
thomasarivera <thomas5info@gmail.com>
2024-03-19 20:52:43 -1000

    Created subscription page

---
+++ app/imports/ui/pages/SubscriptionPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const SubscriptionPage = () => (
+  <Container>
+    <h1>Subscriptions</h1>
+  </Container>
+);
+
+export default SubscriptionPage;
