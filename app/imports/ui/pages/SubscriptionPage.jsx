fb1df187c2fe8f70e346e6878d92256ab6109a76
thomasarivera <thomas5info@gmail.com>
2024-03-20 16:27:29 -1000

    Added links to Navbar and Footer

--- app/imports/ui/pages/SubscriptionPage.jsx
+++ app/imports/ui/pages/SubscriptionPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Image, Card } from 'react-bootstrap';
 
 const SubscriptionPage = () => (
-  <Container>
+  <Container fluid>
+    <Row>
+      <Col className="landing-background">
+        <h1>It Takes a Village to Drive Positive Change.</h1>
+      </Col>
+    </Row>
     <Container className="py-3 align-content-center text-center">
       <Row>
         <h1>Help Grow the Voluntree</h1>
-        <h1>It Takes a Village to Drive Positive Change.</h1>
         <p>At Voluntree, we believe in the power of collective action to drive positive change.
           By donating, you&apos;re not just supporting us: you&apos;re enabling change. Your contribution
           helps us maintain and imporve our platform.
