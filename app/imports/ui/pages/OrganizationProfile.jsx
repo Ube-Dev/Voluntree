fddd76a0192ee562a6f94886608f25d41fea292c
aldenparoni <aparoni@hawaii.edu>
2024-04-08 11:44:47 -1000

    Continued work on profile

--- app/imports/ui/pages/OrganizationProfile.jsx
+++ app/imports/ui/pages/OrganizationProfile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Container, Row, Col, Card, Image } from 'react-bootstrap';
+import { Container, Button, Row, Col, Card, Image } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { useParams } from 'react-router';
 import { Organization } from '../../api/organization/OrganizationCollection';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { PAGE_IDS } from '../utilities/PageIDs';
+import '../css/OrganizationProfile.css';
 
 const OrganizationProfile = () => {
   const { _id } = useParams();
@@ -31,7 +32,7 @@ const OrganizationProfile = () => {
     <Container id={PAGE_IDS.ORGANIZATION_PROFILE}>
       <Row className="py-5">
         <Col md={6}>
-          <h2>{orgProfile.name}</h2>
+          <h1>{orgProfile.name}</h1>
           <Card className="d-flex justify-content-center">
             <Card.Body className="mt-2">
               <h5>Address: {orgProfile.address}</h5>
@@ -39,23 +40,31 @@ const OrganizationProfile = () => {
           </Card>
         </Col>
         <Col md={3}>
-          <Image src={orgProfile.image} />
+          <Image src={orgProfile.image} className="org-profile-img" />
         </Col>
       </Row>
     </Container>
   ) : (
     <Container id={PAGE_IDS.ORGANIZATION_PROFILE}>
       <Row className="py-5">
-        <Col md={6}>
-          <h2>{orgProfile.name}</h2>
+        <Col md={2} />
+        <Col md={3}>
+          <h1>{orgProfile.name}</h1>
+          <Image src={orgProfile.image} className="org-profile-img pb-4" />
+        </Col>
+        <Col md={5}>
           <Card className="d-flex justify-content-center">
             <Card.Body className="mt-2">
               <h5>Address: N/A</h5>
+              <h5>Mission:</h5>
+              <p>{orgProfile.mission}</p>
+              <h5>Contact Email: {orgProfile.contactEmail}</h5>
+              <h5>Phone: {orgProfile.phone}</h5>
             </Card.Body>
+            <Card.Footer>
+              <Button variant="primary" href={`/edit-organization-profile/${_id}`}>Edit</Button>
+            </Card.Footer>
           </Card>
-        </Col>
-        <Col md={3}>
-          <Image src={orgProfile.image} />
         </Col>
       </Row>
     </Container>
