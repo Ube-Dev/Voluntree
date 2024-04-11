19901a4f0ab0dee68f0cde576e819d33acb0d8f6
aldenparoni <aparoni@hawaii.edu>
2024-04-10 23:07:52 -1000

    Continued edits to OrganizationProfile.jsx

--- app/imports/ui/pages/OrganizationProfile.jsx
+++ app/imports/ui/pages/OrganizationProfile.jsx
@@ -6,6 +6,7 @@ import { Organization } from '../../api/organization/OrganizationCollection';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import '../css/OrganizationProfile.css';
+import OrganizationEvents from '../components/OrganizationEvents';
 
 const OrganizationProfile = () => {
   const { _id } = useParams();
@@ -33,39 +34,76 @@ const OrganizationProfile = () => {
       <Row className="py-5">
         <Col md={6}>
           <h1>{orgProfile.name}</h1>
+          <Image src={orgProfile.image} className="org-profile-img rounded mb-4" />
+        </Col>
+        <Col md={6}>
           <Card className="d-flex justify-content-center">
+            <Card.Header>
+              <h3>Organization Details</h3>
+            </Card.Header>
             <Card.Body className="mt-2">
-              <h5>Address: {orgProfile.address}</h5>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={3}>
-          <Image src={orgProfile.image} className="org-profile-img" />
-        </Col>
-      </Row>
-    </Container>
-  ) : (
-    <Container id={PAGE_IDS.ORGANIZATION_PROFILE}>
-      <Row className="py-5">
-        <Col md={2} />
-        <Col md={3}>
-          <h1>{orgProfile.name}</h1>
-          <Image src={orgProfile.image} className="org-profile-img pb-4" />
-        </Col>
-        <Col md={5}>
-          <Card className="d-flex justify-content-center">
-            <Card.Body className="mt-2">
-              <h5>Address: N/A</h5>
               <h5>Mission:</h5>
               <p>{orgProfile.mission}</p>
-              <h5>Contact Email: {orgProfile.contactEmail}</h5>
-              <h5>Phone: {orgProfile.phone}</h5>
+              <h5>Location:</h5>
+              <p>{orgProfile.address} {orgProfile.city}, {orgProfile.state}</p>
+              <Row>
+                <Col>
+                  <h5>Contact Email:</h5>
+                  <p>{orgProfile.contactEmail}</p>
+                </Col>
+                <Col>
+                  <h5>Phone:</h5>
+                  <p>{orgProfile.phone}</p>
+                </Col>
+              </Row>
             </Card.Body>
             <Card.Footer>
               <Button variant="primary" href={`/edit-organization-profile/${_id}`}>Edit</Button>
             </Card.Footer>
           </Card>
         </Col>
+      </Row>
+      <Row>
+        <h1 className="text-center">Events Organized by {orgProfile.name}</h1>
+        <OrganizationEvents org={orgProfile} />
+      </Row>    </Container>
+  ) : (
+    <Container id={PAGE_IDS.ORGANIZATION_PROFILE}>
+      <Row className="py-5">
+        <Col md={6}>
+          <h1>{orgProfile.name}</h1>
+          <Image src={orgProfile.image} className="org-profile-img rounded mb-4" />
+        </Col>
+        <Col md={6}>
+          <Card className="d-flex justify-content-center">
+            <Card.Header>
+              <h3>Organization Details</h3>
+            </Card.Header>
+            <Card.Body className="mt-2">
+              <h5>Mission:</h5>
+              <p>{orgProfile.mission}</p>
+              <h5>Location:</h5>
+              <p>N/A</p>
+              <Row>
+                <Col>
+                  <h5>Contact Email:</h5>
+                  <p>{orgProfile.contactEmail}</p>
+                </Col>
+                <Col>
+                  <h5>Phone:</h5>
+                  <p>{orgProfile.phone}</p>
+                </Col>
+              </Row>
+            </Card.Body>
+            <Card.Footer>
+              <Button variant="primary" href={`/edit-organization-profile/${_id}`}>Edit</Button>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <h1 className="text-center">Events Organized by {orgProfile.name}</h1>
+        <OrganizationEvents org={orgProfile} />
       </Row>
     </Container>
   );
