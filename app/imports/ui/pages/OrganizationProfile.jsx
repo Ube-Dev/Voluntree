f8f7095d24b94605eeee33cbfc000daecdcfd34e
aldenparoni <aparoni@hawaii.edu>
2024-04-12 16:29:26 -1000

    Simplify render of org profile view, add link to org profile in Event Card

--- app/imports/ui/pages/OrganizationProfile.jsx
+++ app/imports/ui/pages/OrganizationProfile.jsx
@@ -22,17 +22,18 @@ const OrganizationProfile = () => {
 
   if (!ready) {
     return (
-      <Container className="p-2">
+      <Container className="px-2 py-5">
         <LoadingSpinner /> {/* Show loading spinner while data is loading */}
       </Container>
     );
   }
 
   // Once data is ready, render the organization profile
-  return orgProfile.hasPhysicalAddress ? (
+  return (
     <Container id={PAGE_IDS.ORGANIZATION_PROFILE}>
       <Row className="py-5">
-        <Col md={6}>
+        <Col md={1} />
+        <Col md={4}>
           <h1>{orgProfile.name}</h1>
           <Image src={orgProfile.image} className="org-profile-img rounded mb-4" />
         </Col>
@@ -43,56 +44,25 @@ const OrganizationProfile = () => {
             </Card.Header>
             <Card.Body className="mt-2">
               <h5>Mission:</h5>
-              <p>{orgProfile.mission}</p>
+              {orgProfile.mission ? (
+                <p>{orgProfile.mission}</p>
+              ) : (<p>N/A</p>)}
               <h5>Location:</h5>
-              <p>{orgProfile.address} {orgProfile.city}, {orgProfile.state}</p>
+              {orgProfile.hasPhysicalAddress ? (
+                <p>{orgProfile.address}, {orgProfile.city}, {orgProfile.state}, {orgProfile.country} {orgProfile.zipCode}</p>
+              ) : (<p>N/A</p>)}
               <Row>
                 <Col>
                   <h5>Contact Email:</h5>
-                  <p>{orgProfile.contactEmail}</p>
+                  {orgProfile.contactEmail ? (
+                    <p>{orgProfile.contactEmail}</p>
+                  ) : (<p>N/A</p>)}
                 </Col>
                 <Col>
                   <h5>Phone:</h5>
-                  <p>{orgProfile.phone}</p>
-                </Col>
-              </Row>
-            </Card.Body>
-            <Card.Footer>
-              <Button variant="primary" href={`/edit-organization-profile/${_id}`}>Edit</Button>
-            </Card.Footer>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <h1 className="text-center">Events Organized by {orgProfile.name}</h1>
-        <OrganizationEvents org={orgProfile} />
-      </Row>
-    </Container>
-  ) : (
-    <Container id={PAGE_IDS.ORGANIZATION_PROFILE}>
-      <Row className="py-5">
-        <Col md={6}>
-          <h1>{orgProfile.name}</h1>
-          <Image src={orgProfile.image} className="org-profile-img rounded mb-4" />
-        </Col>
-        <Col md={6}>
-          <Card className="d-flex justify-content-center">
-            <Card.Header>
-              <h3>Organization Details</h3>
-            </Card.Header>
-            <Card.Body className="mt-2">
-              <h5>Mission:</h5>
-              <p>{orgProfile.mission}</p>
-              <h5>Location:</h5>
-              <p>N/A</p>
-              <Row>
-                <Col>
-                  <h5>Contact Email:</h5>
-                  <p>{orgProfile.contactEmail}</p>
-                </Col>
-                <Col>
-                  <h5>Phone:</h5>
-                  <p>{orgProfile.phone}</p>
+                  {orgProfile.phone ? (
+                    <p>{orgProfile.phone}</p>
+                  ) : (<p>N/A</p>)}
                 </Col>
               </Row>
             </Card.Body>
