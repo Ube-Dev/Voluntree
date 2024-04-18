005f6ed7c25b70d3c9865e4acece1686ef838472
thomasarivera <thomas5info@gmail.com>
2024-04-17 20:19:25 -1000

    Implemented mockup bar graph

--- app/imports/ui/components/AdminHome.jsx
+++ app/imports/ui/components/AdminHome.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { Container, Card } from 'react-bootstrap';
-import LoadingSpinner from './LoadingSpinner';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import AdminStats from './AdminStats';
 
-const AdminHome = () => {
-
-  const ready = true; // Placeholder for now
-
-  return ready ? (
-    <Container>
-      <Card>
-        <Card.Header>
-          <h2>Admin Overview</h2>
-        </Card.Header>
-      </Card>
-    </Container>
-  ) : (
-    <Container className="p-2">
-      <LoadingSpinner />
-    </Container>
-  );
-};
+const AdminHome = () => (
+  <Container className="py-3">
+    <Row className="text-center py-3">
+      <h1>Admin View</h1>
+    </Row>
+    <Row>
+      <Col>
+        <Card className="rounded-4">
+          <Card.Header><h3>Site Management</h3></Card.Header>
+          <Col>
+            <Button>Events</Button>
+            <Button>Organization</Button>
+            <Button>Users</Button>
+            <Button>Reviews</Button>
+          </Col>
+        </Card>
+      </Col>
+      <Col>
+        <AdminStats />
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default AdminHome;
---
+++ app/imports/ui/components/AdminStats.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import faker from 'faker';
+
+const AdminStats = () => {
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  );
+
+  const options = {
+    indexAxis: 'y',
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'right',
+      },
+    },
+  };
+
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Hours Served',
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        backgroundColor: 'rgba(255, 99, 132)',
+      },
+    ],
+  };
+
+  return (
+    <Container>
+      <Card className="p-3 rounded-4">
+        <Row>
+          <Col className="text-center">
+            <h3>Total Hours Served</h3>
+            <Bar options={options} data={data} />
+          </Col>
+        </Row>
+      </Card>
+    </Container>
+  );
+};
+
+export default AdminStats;
