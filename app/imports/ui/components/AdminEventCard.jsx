6643094e3aee8c02986229998e79fc3b388db966
thomasarivera <thomas5info@gmail.com>
2024-04-22 07:25:43 -1000

    Implemented Admin event view

---
+++ app/imports/ui/components/AdminEventCard.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Row, Col, Button } from 'react-bootstrap';
+import { PersonFill } from 'react-bootstrap-icons';
+import '../css/EventCard.css';
+import { Link } from 'react-router-dom';
+import { COMPONENT_IDS } from '../utilities/ComponentIDs';
+
+const AdminEventCard = ({ event }) => (
+  <Link className="text-decoration-none" to={`/view_event/${event._id}`}>
+    <Card key={event._id} className="event-card">
+      <Card.Header>
+        <Card.Img src={event.image} className="event-image rounded-3" />
+      </Card.Header>
+      <Card.Body>
+        <Card.Title className="event-title">{event.title}</Card.Title>
+        <hr />
+        <Card.Text>{event.city}</Card.Text>
+        <Card.Text className="event-description">{event.description}</Card.Text>
+        <Link className="host-link" to={`/org-profile/${event.hostID}`}><Card.Text>{event.hostBy}</Card.Text></Link>
+        <Button variant="primary" href={`/edit-event/${event._id}`} id={COMPONENT_IDS.ORG_EVENT_CARD_EDIT}>Edit</Button>
+      </Card.Body>
+      <Card.Footer>
+        <Row className="event-footer">
+          <Col className="text-start col-8">
+            <small>{event.startTime.toDateString()} | {event.startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}</small>
+          </Col>
+          <Col className="text-end col-4">
+            <small><PersonFill /> {event.spotsFilled.length}/{event.totalSpots}</small>
+          </Col>
+        </Row>
+      </Card.Footer>
+    </Card>
+  </Link>
+);
+
+AdminEventCard.propTypes = {
+  event: PropTypes.shape({
+    title: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+    city: PropTypes.string,
+    startTime: PropTypes.instanceOf(Date),
+    time: PropTypes.instanceOf(Date),
+    totalSpots: PropTypes.number,
+    spotsFilled: PropTypes.instanceOf(Array),
+    frequency: PropTypes.string,
+    accessibilities: PropTypes.instanceOf(Array),
+    requirements: PropTypes.instanceOf(Array),
+    impact: PropTypes.string,
+    hostBy: PropTypes.string,
+    hostID: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default AdminEventCard;
--- app/imports/ui/pages/AdminEventModeration.jsx
+++ app/imports/ui/pages/AdminEventModeration.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Events } from '../../api/event/EventCollection';
+import { MainCategory } from '../../api/category/MainCategoryCollection';
+import AdminEventFilter from '../components/EventFilter';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 const AdminEventModeration = () => {
-  return (
-    <div>
-      <h1>Admin Event Moderation</h1>
-    </div>
+
+  const { ready, events, categories } = useTracker(() => {
+    // Get access to events
+    const subscription = Events.subscribeEvent();
+    const subscription2 = MainCategory.subscribeMainCategory();
+    // Make sure its ready
+    const rdy = subscription.ready() && subscription2.ready();
+    // fetch all events and categories
+    const theEvents = Events.find({}).fetch().reverse();
+    const theCategories = MainCategory.find({}).fetch();
+    return {
+      events: theEvents,
+      categories: theCategories,
+      ready: rdy,
+    };
+  }, []);
+
+  return ready ? (
+    <Container fluid className="color1">
+      <Container>
+        <Row>
+          <Col>
+            <h1>Event Moderation</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card>
+              <AdminEventFilter event={events} categories={categories} />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  ) : (
+    <LoadingSpinner />
   );
-}
+};
 
 export default AdminEventModeration;
