e20335fadfe23e85ae93bb9124d3a90d5c7e3bf1
Ryan <ryanseng32@gmail.com>
2024-04-30 21:25:27 -1000

    Revert "Merge branch 'main' of https://github.com/Ube-Dev/Voluntree"
    This reverts commit 2199877640a4f8497ecba40c4e2681b7448c974d, reversing
    changes made to a43d11bcd34ba8a20e52d66963e203cf543a981d.

--- app/imports/ui/components/UserDashboard.jsx
+++ app/imports/ui/components/UserDashboard.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Row, Col, Card, Image, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Image, Button } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { UserProfiles } from '../../api/user/UserProfileCollection';
 import LoadingSpinner from './LoadingSpinner';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
-
-// This component displays the user's dashboard, which includes their profile picture, name, and total hours recorded.
-// It also includes buttons to view and edit the user's profile, as well as to log hours.
 
 const UserDashboard = () => {
   const { ready, userProfile } = useTracker(() => {
@@ -21,33 +18,35 @@ const UserDashboard = () => {
   });
 
   return ready ? (
-    <Card className="rounded-4">
-      <Card.Header>
-        <h2>Overview</h2>
-      </Card.Header>
-      <Card.Body className="d-flex flex-column align-items-start">
-        <Row>
-          <Col>
-            <Image src={userProfile?.image || '/path/to/default/profile/image.png'} alt="Profile Image" id="profile-img" />
-          </Col>
-          <Col>
-            <h4>{userProfile.firstName} {userProfile.lastName}</h4>
-            <p>Hours Recorded: {userProfile.totalHours}</p>
-          </Col>
-        </Row>
-      </Card.Body>
-      <Card.Footer className="p-2">
-        <Row>
-          <Col className="text-start">
-            <Button id={COMPONENT_IDS.USER_DASHBOARD_VIEW_PROFILE} className="mx-1" href="/profile">View Profile</Button>
-            <Button id={COMPONENT_IDS.USER_DASHBOARD_EDIT_PROFILE} className="mx-1" href={`/edit-user-profile/${userProfile?._id}`}>Edit</Button>
-          </Col>
-          <Col className="text-end">
-            <Button id={COMPONENT_IDS.USER_DASHBOARD_VIEW_QR_CODE} className="mx-1" href={`/qr-code/${userProfile?._id}`}>Log Hours</Button>
-          </Col>
-        </Row>
-      </Card.Footer>
-    </Card>
+    <Container>
+      <Card>
+        <Card.Header>
+          <h2>Overview</h2>
+        </Card.Header>
+        <Card.Body className="d-flex flex-column align-items-start">
+          <Row>
+            <Col>
+              <Image src={userProfile?.image || '/path/to/default/profile/image.png'} alt="Profile Image" id="profile-img" />
+            </Col>
+            <Col>
+              <h4>{userProfile.firstName} {userProfile.lastName}</h4>
+              <p>Hours Recorded: {userProfile.totalHours}</p>
+            </Col>
+          </Row>
+        </Card.Body>
+        <Card.Footer className="p-2">
+          <Row>
+            <Col className="text-start">
+              <Button id={COMPONENT_IDS.USER_DASHBOARD_VIEW_PROFILE} className="mx-1" href="/profile">View Profile</Button>
+              <Button id={COMPONENT_IDS.USER_DASHBOARD_EDIT_PROFILE} className="mx-1" href={`/edit-user-profile/${userProfile?.id}`}>Edit</Button>
+            </Col>
+            <Col className="text-end">
+              <Button id={COMPONENT_IDS.USER_DASHBOARD_VIEW_QR_CODE} className="mx-1" href={`/qr-code/${userProfile?._id}`}>Log Hours</Button>
+            </Col>
+          </Row>
+        </Card.Footer>
+      </Card>
+    </Container>
   ) : (
     <LoadingSpinner />
   );
