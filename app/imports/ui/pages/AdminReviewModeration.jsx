a1e712ec5ebad5cfe2636b75647365ed80f26366
thomasarivera <thomas5info@gmail.com>
2024-05-01 09:10:25 -1000

    Implemented search

--- app/imports/ui/pages/AdminReviewModeration.jsx
+++ app/imports/ui/pages/AdminReviewModeration.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Card, ButtonGroup, Button, Modal } from 'react-bootstrap';
+import { Container, Row, Col, Card, ButtonGroup, Button, Modal, Form } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table';
+import Fuse from 'fuse.js';
 import '../css/AdminModeration.css';
 import { useTracker } from 'meteor/react-meteor-data';
 import swal from 'sweetalert';
@@ -9,6 +10,7 @@ import { Review } from '../../api/review/ReviewCollection';
 import { UserProfiles } from '../../api/user/UserProfileCollection';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { removeReview } from '../../startup/both/Methods';
+import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 
 const AdminReviewModeration = () => {
   // Delete function variable definitions
@@ -35,8 +37,6 @@ const AdminReviewModeration = () => {
     return user ? `${user.firstName} ${user.lastName}` : 'Unknown';
   };
 
-  console.log(reviews);
-
   const confirmDelete = () => {
     Meteor.call(removeReview, reviewToRemove, (error) => {
       if (error) {
@@ -48,41 +48,86 @@ const AdminReviewModeration = () => {
     });
   };
 
+  // Search variable definitions
+  let displayedReviews = reviews;
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchPerformed, setSearchPerformed] = useState(false);
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+    setSearchPerformed(true);
+  };
+
+  if (searchPerformed && searchQuery !== '') {
+    const fuseOptions = {
+      isCaseSensitive: false,
+      shouldSort: true,
+      includeMatches: true,
+      findAllMatches: true,
+      useExtendedSearch: false,
+      threshold: 0.2,
+      keys: ['rating', 'content'],
+    };
+
+    const fuse = new Fuse(reviews, fuseOptions);
+    const result = fuse.search(searchQuery);
+    displayedReviews = result.map((item) => item.item);
+  }
+
   return ready ? (
-    <Container fluid className="color1">
+    <Container fluid className="color1 pb-5">
       <Container>
-        <Row className="text-center">
-          <h1>Admin Review Moderation</h1>
+        <Row className="text-center py-4">
+          <h1>Review Moderation</h1>
         </Row>
         <Row>
           <Col>
             <Card className="p-3 rounded-4 org-moderation-background">
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Reviewer</th>
-                    <th>Rating</th>
-                    <th>Type</th>
-                    <th>Review</th>
-                    <th>Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {reviews.map((review) => (
-                    <tr key={review._id}>
-                      <td>{getUserById(review.reviewerID)}</td>
-                      <td>{review.reviewFor.type}</td>
-                      <td>{review.rating}</td>
-                      <td>{review.content}</td>
-                      <td className="text-center">
-                        <ButtonGroup>
-                          <Button variant="danger" onClick={() => toggleDeleteConfirmation(review._id)}>Delete</Button>
-                        </ButtonGroup>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
+              <Row className="justify-content-center">
+                <Col className="col-7">
+                  <Form.Group className="search-bar">
+                    <Form.Control
+                      id={COMPONENT_IDS.ADMIN_ORGANIZATION_MODERATION_SEARCH_BAR}
+                      type="text"
+                      placeholder="Search for organizations..."
+                      className="align-content-center"
+                      value={searchQuery}
+                      onChange={handleSearchChange}
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Card.Body>
+                    <Table striped bordered hover>
+                      <thead>
+                        <tr>
+                          <th>Reviewer</th>
+                          <th>Rating</th>
+                          <th>Type</th>
+                          <th>Review</th>
+                          <th>Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {displayedReviews.map((review) => (
+                          <tr key={review._id}>
+                            <td>{getUserById(review.reviewerID)}</td>
+                            <td>{review.reviewFor.type}</td>
+                            <td>{review.rating}</td>
+                            <td>{review.content}</td>
+                            <td className="text-center">
+                              <ButtonGroup>
+                                <Button variant="danger" onClick={() => toggleDeleteConfirmation(review._id)}>Delete</Button>
+                              </ButtonGroup>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
+                  </Card.Body>
+                </Col>
+              </Row>
             </Card>
           </Col>
         </Row>
